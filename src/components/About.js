cf2b1f29fb102aae257ff248cd05edda391b9ca8
Aidan <adn.mck@gmail.com>
2023-02-12 15:55:06 +0000

    added new sentence in About

--- src/components/About.js
+++ src/components/About.js
@@ -16,10 +16,14 @@ function About() {
         `I dabbled with C, Python, SQLite, and Lua during Covid lockdowns when I completed Harvard's CS50 course which made me realise how much passion and energy I have for programming, and I decided to focus on a career change.`;
 
     const three = document.createElement('p');
-    three.textContent =
+    three.textContent = 
+        `I have mainly been building and maintaining my projects using JavaScript, npm, Webpack, Git, and Linux. I am currently learning React, and plan to learn more frameworks and languages as I continue my journey as I believe that keeping up to date with new technologies is essential, and learning should never end.`;
+    
+    const four = document.createElement('p');
+    four.textContent =
         `My current goal is to find a role where I can begin my career and continue developing my skillset within a professional environment while doing a job that I am incredibly passionate about.`;
-
-    let sentences = [one, two, three];
+    
+    let sentences = [one, two, three, four];
 
     sentences.forEach(sentence => {
         about.append(sentence);
